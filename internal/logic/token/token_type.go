5e1f6da85f529ee7db006778bd1be0436d226249
Chris Turner <j.chris.turner@gmail.com>
2023-01-29 11:05:02 -0800

    Use atomic custom token type

--- internal/logic/token/token_type.go
+++ internal/logic/token/token_type.go
@@ -1,8 +1,24 @@
 package token
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
 type TokenType int
+
+func (t TokenType) String() string {
+	customTT, found := globalRegistry.Lookup(t)
+	if !found {
+		panic(fmt.Sprintf("unrecognized TokenType with value %d", t))
+	}
+	return customTT
+}
+
+func (t TokenType) IsCustom() bool {
+	return t >= TTcustom
+}
 
 const (
 	TTunknown TokenType = iota
@@ -18,46 +34,53 @@ const (
 	// Any custom types should be > 999
 )
 
-var globalCustomTokenTypeStringRegistry = make(map[TokenType]string)
-var customIdx = TTcustom
+func NewRegistry() *Registry {
+	tr := Registry{idx: TTcustom}
+	baseRegistry := map[TokenType]string{
+		TTunknown:         "TT.Unknown",
+		TTws:              "TT.WS",
+		TTnl:              "TT.NL",
+		TTcontent:         "TT.Content",
+		TTcodeGlobalBlock: "TT.CodeGlobalBlock",
+		TTcodeLocalBlock:  "TT.CodeLocalBlock",
+		TTcodeLocalExpr:   "TT.CodeLocalExpr",
+		TTmacro:           "TT.Macro",
+		TTcustom:          "TT.Custom",
+	}
+	tr.registry.Store(baseRegistry)
+	return &tr
+}
 
-func RegisterCustomTokenType(s string) TokenType {
-	customIdx++
-	tt := TokenType(customIdx)
-	globalCustomTokenTypeStringRegistry[tt] = `TT.` + s
+type Registry struct {
+	mu       sync.Mutex
+	idx      int // only change with mutex
+	registry atomic.Value
+}
+
+func (tr *Registry) Lookup(tt TokenType) (string, bool) {
+	m := tr.registry.Load().(map[TokenType]string)
+	s, found := m[tt]
+	return s, found
+}
+
+func (tr *Registry) Register(name string) TokenType {
+	tr.mu.Lock()
+	defer tr.mu.Unlock()
+
+	tr.idx++
+	tt := TokenType(tr.idx)
+	m1 := tr.registry.Load().(map[TokenType]string)
+	m2 := make(map[TokenType]string)
+	for k, v := range m1 {
+		m2[k] = v
+	}
+	m2[tt] = `TT.` + name
+	tr.registry.Store(m2)
 	return tt
 }
 
-func (t TokenType) String() string {
-	switch t {
-	case TTunknown:
-		return "TT.Unknown"
-	case TTws:
-		return "TT.WS"
-	case TTnl:
-		return "TT.NL"
-	case TTcontent:
-		return "TT.Content"
-	case TTcodeGlobalBlock:
-		return "TT.CodeGlobalBlock"
-	case TTcodeLocalBlock:
-		return "TT.CodeLocalBlock"
-	case TTcodeLocalExpr:
-		return "TT.CodeLocalExpr"
-	case TTmacro:
-		return "TT.Macro"
-	case TTcustom:
-		return "TT.Custom"
-	default:
-		customTT, found := globalCustomTokenTypeStringRegistry[t]
-		if found {
-			return customTT
-		}
-		panic(fmt.Sprintf("unrecognized TokenType with value %d", t))
+var globalRegistry = NewRegistry()
 
-	}
-}
-
-func (t TokenType) IsCustom() bool {
-	return t >= TTcustom
+func RegisterCustomTokenType(name string) TokenType {
+	return globalRegistry.Register(name)
 }
